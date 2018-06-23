fdbb01ec6c440f301643150b0e1e69e7f575ad9d
saicheong <yow.saicheong@gmail.com>
2018-06-23 08:40:13 +0800

    Move grid components to own namespace and simplify code for clarity

--- src/cljs/conway/core.cljs
+++ src/cljs/conway/core.cljs
@@ -18,7 +18,7 @@
     [reagent.core :as reagent]
     [re-frame.core :as rf]
     [conway.config :as config]
-    [clojure.set :as set]))
+    [conway.grid :refer [svg-grid svg-grid2 canvas-grid]]))
 
 (defn neighbours [[x y]]
   (for [dx [-1 0 1]
@@ -145,225 +145,6 @@
 
 ;; -- Domino 5 - View Functions ----------------------------------------------
 
-(defn client-xy [ev]
-  [(.-clientX ev) (.-clientY ev)])
-
-(defn svg-xy-fn
-  "Create the browser coordinates to SVG cordinates transformation function for
-   the SVG element
-
-   Reference:
-   https://stackoverflow.com/questions/12752519/svg-capturing-mouse-coordinates"
-  [svg-elem]
-  (let [svg-point (.createSVGPoint svg-elem)]
-    (fn [[x y]]
-      (let [matrix (-> svg-elem .getScreenCTM .inverse)]
-        (set! (.-x svg-point) x)
-        (set! (.-y svg-point) y)
-        (let [gpt (.matrixTransform svg-point matrix)]
-          [(int (.-x gpt)) (int (.-y gpt))])))))
-
-(defn grid-xy-fn
-  "Create the SVG coordinates to grid coordinates transformation function for
-   the given grid size"
-  [cell-sz]
-  (fn [svg-xy] (mapv #(.floor js/Math (/ % cell-sz)) svg-xy)))
-
-(defn svg-cell
-  ([loc sz]
-   (let [[x y] loc
-         w (dec sz)]
-     [:rect {:key    (str x "," y)
-             :x      (* x sz)
-             :y      (* y sz)
-             :height w
-             :width  w}]))
-  ([loc sz style]
-   (assoc-in (svg-cell loc sz) [1 :style] style)))
-
-(defn svg-grid
-  "A React/Reagent component that renders life
-   to an SVG element.
-
-   Uses a ref function to capture the backing svg element - needed to convert
-   the mouse location to svg coordinates.
-
-   Reference:
-   https://github.com/reagent-project/reagent/blob/master/doc/CreatingReagentComponents.md
-   https://presumably.de/reagent-mysteries-part-3-manipulating-the-dom.html
-   http://timothypratley.blogspot.com/2017/01/reagent-deep-dive-part-2-lifecycle-of.html
-   "
-  [width height cell-size]
-  (let [svg-xy (atom nil)]
-    (fn [width height cell-size]
-      (let [live-cells @(rf/subscribe [:live-cells])
-            mouse-over @(rf/subscribe [:mouse-over])
-            grid-xy (grid-xy-fn cell-size)]
-        [:svg {:key            "grid"
-               :style          {:border-style "solid"
-                                :border-width "1px"
-                                :margin       "10px"}
-               :width          width
-               :height         height
-               :ref            (fn [svg-elem]
-                                 (when (nil? @svg-xy)
-                                   (reset! svg-xy (svg-xy-fn svg-elem))))
-
-               :on-click       (fn [ev]
-                                 (rf/dispatch [:toggle-cell (-> ev client-xy (@svg-xy) grid-xy)]))
-
-               :on-mouse-move  (fn [ev]
-                                 (rf/dispatch [:mouse-over (-> ev client-xy (@svg-xy) grid-xy)]))
-
-               :on-mouse-leave (fn [_]
-                                 (rf/dispatch [:mouse-out]))}
-
-         ;; draw the life cells
-         (for [loc live-cells]
-           (svg-cell loc cell-size))
-
-         ;; mark the moused-over cell location
-         (if-let [loc mouse-over]
-           (svg-cell loc cell-size {:stroke         "red"
-                                    :stroke-width   "2"
-                                    :stroke-opacity "0.3"
-                                    :fill-opacity   "0"}))
-
-         ])))
-
-  )
-
-(defn svg-grid2
-  "Same as svg-grid, but implemented as a form 3 component, so that the svg element
-   is captured when component is mounted"
-  [width height cell-size]
-  (let [svg-xy (atom nil)]
-    (reagent/create-class
-      {:display-name
-       "svg-grid2"
-
-       :component-did-mount
-       (fn [this]
-         (println "SVG2 did mount")
-         (let [svg-elem (reagent/dom-node this)]
-           (reset! svg-xy (svg-xy-fn svg-elem))))
-
-       :reagent-render
-       (fn [width height cell-size]
-         (let [live-cells @(rf/subscribe [:live-cells])
-               mouse-over @(rf/subscribe [:mouse-over])
-               grid-xy (grid-xy-fn cell-size)]
-           [:svg {:key            "grid2"
-                  :style          {:border-style "solid"
-                                   :border-width "1px"
-                                   :margin       "10px"}
-                  :width          width
-                  :height         height
-
-                  :on-click       (fn [ev]
-                                    (rf/dispatch [:toggle-cell (-> ev client-xy (@svg-xy) grid-xy)]))
-
-                  :on-mouse-move  (fn [ev]
-                                    (rf/dispatch [:mouse-over (-> ev client-xy (@svg-xy) grid-xy)]))
-
-                  :on-mouse-leave (fn [_]
-                                    (rf/dispatch [:mouse-out]))}
-            (for [loc live-cells]
-              (svg-cell loc cell-size {:fill         "grey"
-                                       :fill-opacity 0.5}))
-
-            (if-let [loc mouse-over]
-              (svg-cell loc cell-size {:stroke         "blue"
-                                       :stroke-width   "2"
-                                       :stroke-opacity "0.3"
-                                       :fill-opacity   "0"}))
-            ]))
-
-       })))
-
-
-(defn canvas-xy-fn
-  "Returns a function to translate browser coordinate to canvas coordinate"
-  [elem]
-  (fn [[x y]]
-    (let [rect (.getBoundingClientRect elem)
-          cx (- x (.-left rect))
-          cy (- y (.-top rect))]
-      [cx cy])))
-
-(defn canvas-grid-xy-fn
-  "Returns a function to translate canvas coordinate to game grid coordinate"
-  [cell-size]
-  (fn [xy]
-    (mapv #(.floor js/Math (/ % cell-size)) xy)))
-
-(defn draw-cell [ctx [x y] sz]
-  (let [w (dec sz)]
-    (.fillRect ctx (* x sz) (* y sz) w w)))
-
-(defn canvas-grid
-  "A Reagent/React component that renders life to a html canvas"
-  [width height cell-size]
-  (let [elem (atom nil)
-        canvas-cells (atom nil)
-        live-cells (atom nil)
-        cell-size (atom cell-size)
-
-        canvas-xy (atom nil)
-        canvas-grid-xy (canvas-grid-xy-fn @cell-size)
-        render (fn [sz]
-                 (let [ctx (.getContext @elem "2d")
-                       cells @live-cells
-                       old-cells @canvas-cells
-                       new-cells (set/difference cells old-cells)
-                       dead-cells (set/difference old-cells cells)]
-
-                   ;; draw new cells
-                   (set! (.-fillStyle ctx) "grey")
-                   (doseq [pos new-cells]
-                     (draw-cell ctx pos sz))
-
-                   ;; draw over dead cells
-                   (set! (.-fillStyle ctx) "white")
-                   (doseq [pos dead-cells]
-                     (draw-cell ctx pos sz))
-
-                   ;; remember cells drawn on canvas
-                   (reset! canvas-cells cells)))]
-
-    (reagent/create-class
-      {:display-name
-       "canvas-grid"
-
-       :component-did-mount
-       (fn [this]
-         (println "canvas did mount")
-         (reset! elem (reagent/dom-node this))
-         (reset! canvas-xy (canvas-xy-fn @elem))
-         (render @cell-size))
-
-       :reagent-render
-       (fn [wd ht sz]
-         (let []
-           (reset! live-cells @(rf/subscribe [:live-cells]))
-           (reset! cell-size sz)
-           [:canvas {:key            "canvas"
-                     :style          {:border-style "solid"
-                                      :border-width "1px"
-                                      :margin       "10px"}
-                     :width          wd
-                     :height         ht
-                     :on-click       (fn [ev]
-                                       (rf/dispatch [:toggle-cell (-> ev client-xy (@canvas-xy) canvas-grid-xy)]))
-                     :on-mouse-move  (fn [ev]
-                                       (rf/dispatch [:mouse-over (-> ev client-xy (@canvas-xy) canvas-grid-xy)]))
-                     :on-mouse-leave (fn [_]
-                                       (rf/dispatch [:mouse-out]))}]))
-
-       :component-did-update
-       (fn [_]
-         (render @cell-size))})))
-
 (defn button [label event]
   ;; Have to return a function for this to work
   (fn []
@@ -391,14 +172,14 @@
            :on-change (fn [e]
                         (rf/dispatch [:interval-change (-> e .-target .-value)]))}])
 
-(defn grid-pos []
+(defn pointer-pos []
   (let [pos @(rf/subscribe [:mouse-over])]
     [:div "Pointer Position: " (str pos)]))
 
 (defn main-panel []
   [:div
    [:h1 "Conway Game of Life - using Re-frame"]
-   [:div {:style {:margin-bottom "10px"}} [grid-pos]]
+   [:div {:style {:margin-bottom "10px"}} [pointer-pos]]
    [:div
     [button " Reset " :initialize-db]
     [step-btn]
@@ -407,9 +188,32 @@
     "  Interval: "
     [interval-input]
     ]
-   [:div [svg-grid 800 500 10]]
-   #_[:div {:style {:margin-top "10px"}} [svg-grid2 800 500 10]]
-   #_[:div {:style {:margin-top "10px"}} [canvas-grid 800 500 10]]])
+
+
+   [:div [svg-grid {:width     800
+                    :height    500
+                    :cell-size 10
+                    :style     {:border-style "solid"
+                                :border-width "3px"
+                                :border-color "#bccad6"
+                                :margin       "10px"}}]]
+
+   [:div [svg-grid2 {:width     800
+                    :height    500
+                    :cell-size 10
+                    :style     {:border-style "solid"
+                                :border-width "3px"
+                                :border-color "#622569"
+                                :margin       "10px"}}]]
+
+   [:div [canvas-grid {:width     800
+                       :height    500
+                       :cell-size 10
+                       :style     {:border-style "solid"
+                                   :border-color "#588c7e"
+                                   :border-width "3px"
+                                   :margin       "10px"}}]]
+   ])
 
 
 ;; -- Entry Point -------------------------------------------------------------
---
+++ src/cljs/conway/grid.cljs
@@ -0,0 +1,186 @@
+(ns conway.grid
+  (:require [clojure.set :as set]
+            [re-frame.core :as rf]
+            [reagent.core :as reagent]))
+
+
+(defn- client-pos [ev]
+  [(.-clientX ev) (.-clientY ev)])
+
+(defn- svg-pos [client-pos svg-elem]
+  (let [[x y] client-pos
+        svg-point (.createSVGPoint svg-elem)
+        matrix (-> svg-elem .getScreenCTM .inverse)]
+    (set! (.-x svg-point) x)
+    (set! (.-y svg-point) y)
+    (let [gpt (.matrixTransform svg-point matrix)]
+      [(int (.-x gpt)) (int (.-y gpt))])))
+
+(defn- grid-pos [svg-pos cell-size]
+  (mapv #(.floor js/Math (/ % cell-size)) svg-pos))
+
+(defn svg-grid
+  "A React/Reagent component that renders life
+   to an SVG element.
+
+   Uses a ref function to capture the backing svg element - needed to convert
+   the mouse location to svg coordinates.
+
+   Reference:
+   https://github.com/reagent-project/reagent/blob/master/doc/CreatingReagentComponents.md
+   https://presumably.de/reagent-mysteries-part-3-manipulating-the-dom.html
+   http://timothypratley.blogspot.com/2017/01/reagent-deep-dive-part-2-lifecycle-of.html
+   "
+
+  []
+  (let [elem (atom nil)]
+    (fn [props]
+      (let [{:keys [cell-size] :or {cell-size 10}} props
+            live-cells @(rf/subscribe [:live-cells])
+            mouse-over @(rf/subscribe [:mouse-over])
+            cell (fn [loc props]
+                   (let [[x y] loc
+                         wd (dec cell-size)]
+                     [:rect (merge props
+                                   {:key    (str x "," y)
+                                    :x      (* x cell-size)
+                                    :y      (* y cell-size)
+                                    :height wd
+                                    :width  wd})]))
+            grid-loc (fn [ev]
+                       (-> (client-pos ev)
+                           (svg-pos @elem)
+                           (grid-pos cell-size)))]
+        [:svg (merge (dissoc props :cell-size)              ; remove non-svg attr
+                     {:key            "grid"
+                      :ref            #(reset! elem %)
+                      :on-click       #(rf/dispatch [:toggle-cell (grid-loc %)])
+                      :on-mouse-move  #(rf/dispatch [:mouse-over (grid-loc %)])
+                      :on-mouse-leave #(rf/dispatch [:mouse-out])})
+         ;; draw the life cells
+         (for [loc live-cells]
+           (cell loc {:fill "#667292"}))
+
+         ;; mark the moused-over cell location
+         (if-let [loc mouse-over]
+           (cell loc {:stroke       "#87bdd8"
+                      :fill-opacity "0"}))]))))
+
+(defn svg-grid2
+  "Same as svg-grid, but implemented as a form 3 component, so that the svg element
+   is captured when component is mounted"
+  []
+  (let [elem (atom nil)]
+    (reagent/create-class
+      {:display-name
+       "svg-grid2"
+
+       :component-did-mount
+       (fn [this]
+         (println "SVG2 did mount")
+         (reset! elem (reagent/dom-node this)))
+
+       :reagent-render
+       (fn [props]
+         (let [{:keys [cell-size] :or {cell-size 10}} props
+               live-cells @(rf/subscribe [:live-cells])
+               mouse-over @(rf/subscribe [:mouse-over])
+               cell (fn [loc props]
+                      (let [[x y] loc
+                            wd (dec cell-size)]
+                        [:rect (merge props
+                                      {:key    (str x "," y)
+                                       :x      (* x cell-size)
+                                       :y      (* y cell-size)
+                                       :height wd
+                                       :width  wd})]))
+               grid-loc (fn [ev]
+                          (-> (client-pos ev)
+                              (svg-pos @elem)
+                              (grid-pos cell-size)))]
+           [:svg (merge (dissoc props :cell-size)
+                        {:key            "grid2"
+                         :on-click       #(rf/dispatch [:toggle-cell (grid-loc %)])
+                         :on-mouse-move  #(rf/dispatch [:mouse-over (grid-loc %)])
+                         :on-mouse-leave #(rf/dispatch [:mouse-out])})
+            (for [loc live-cells]
+              (cell loc {:fill         "#c83349"
+                         :fill-opacity 0.5}))
+
+            (if-let [loc mouse-over]
+              (cell loc {:stroke         "#5b9aa0"
+                         :stroke-opacity "0.8"
+                         :fill-opacity   "0"}))]))})))
+
+
+(defn- canvas-pos
+  "Returns a function to translate browser coordinate to canvas coordinate"
+  [client-pos elem]
+  (let [[x y] client-pos
+        rect (.getBoundingClientRect elem)
+        cx (- x (.-left rect))
+        cy (- y (.-top rect))]
+    [cx cy]))
+
+(defn canvas-grid
+  "A Reagent/React component that renders life to a html canvas"
+  [_]
+  (let [elem (atom nil)
+        canvas-cells (atom nil)
+        live-cells (atom nil)
+        cell-size (atom nil)
+
+        grid-loc (fn [ev]
+                   (-> (client-pos ev)
+                       (canvas-pos @elem)
+                       (grid-pos @cell-size)))
+        render (fn []
+                 (let [ctx (.getContext @elem "2d")
+                       cells @live-cells
+                       old-cells @canvas-cells
+                       new-cells (set/difference cells old-cells)
+                       dead-cells (set/difference old-cells cells)
+                       draw-cell (fn [c [x y]]
+                                   (let [sz @cell-size
+                                         wd (dec sz)]
+                                     (.fillRect c (* x sz) (* y sz) wd wd)))]
+
+                   ;; draw new cells
+                   (set! (.-fillStyle ctx) "#ff6f69")
+                   (doseq [pos new-cells]
+                     (draw-cell ctx pos))
+
+                   ;; draw over dead cells
+                   (set! (.-fillStyle ctx) "#ffffff")
+                   (doseq [pos dead-cells]
+                     (draw-cell ctx pos))
+
+                   ;; TODO:
+                   ;; I have not figured out how to draw the mouse over cell
+                   ;; and make sure it does not draw over the live/dead cells
+
+                   ;; remember cells drawn on canvas
+                   (reset! canvas-cells cells)))]
+
+    (reagent/create-class
+      {:display-name
+       "canvas-grid"
+
+       :component-did-mount
+       (fn [this]
+         (println "canvas did mount")
+         (reset! elem (reagent/dom-node this))
+         (render))
+
+       :reagent-render
+       (fn [props]
+         (reset! live-cells @(rf/subscribe [:live-cells]))
+         (reset! cell-size (:cell-size props 10))
+         [:canvas (merge (dissoc props :cell-size)
+                         {:key            "canvas"
+                          :on-click       #(rf/dispatch [:toggle-cell (grid-loc %)])
+                          :on-mouse-move  #(rf/dispatch [:mouse-over (grid-loc %)])
+                          :on-mouse-leave #(rf/dispatch [:mouse-out])})])
+
+       :component-did-update
+       (fn [_] (render))})))
