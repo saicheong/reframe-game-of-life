56ff36fde704fc3db4d07452fd78da2b28d28eef
saicheong <yow.saicheong@gmail.com>
2018-07-02 00:00:51 +0800

    Use shadow-cljs compiler

--- project.clj
+++ project.clj
@@ -1,66 +1,16 @@
 (defproject conway "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [reagent "0.7.0"]
                  [re-frame "0.10.5"]]
-
-  :plugins [[lein-cljsbuild "1.1.7"]]
-
+  :source-paths ["src/clj"]
   :min-lein-version "2.5.3"
 
-  :source-paths ["src/clj"]
-
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
-
-  :figwheel {:css-dirs ["resources/public/css"]}
-
   :profiles
-  {:dev
-   {:dependencies [[binaryage/devtools "0.9.10"]]
-    :plugins      [[lein-figwheel "0.5.16"]]}
-
-   :prod
-   {}}
-
-  :cljsbuild
-  {:builds
-   [{:id           "dev"
-     :source-paths ["src/cljs"]
-     :figwheel     {:on-jsload "conway.core/mount-root"}
-     :compiler     {:main                 conway.core
-                    :output-to            "resources/public/js/compiled/app.js"
-                    :output-dir           "resources/public/js/compiled/out"
-                    :asset-path           "js/compiled/out"
-                    :source-map-timestamp true
-                    :preloads             [devtools.preload]
-                    :external-config      {:devtools/config {:features-to-install :all}}
-                    }}
-
-    {:id           "simple"
-     :source-paths ["src/cljs"]
-     :compiler     {:main            conway.core
-                    :output-to       "resources/public/js/compiled/app.js"
-                    :optimizations   :simple
-                    :closure-defines {goog.DEBUG false}
-                    :pretty-print    false}}
-
-    {:id           "min"
-     :source-paths ["src/cljs"]
-     :compiler     {:main            conway.core
-                    :output-to       "resources/public/js/compiled/app.js"
-                    :optimizations   :advanced
-                    :closure-defines {goog.DEBUG false}
-                    :pretty-print    false}}
-
-    {:id           "min-debug"
-     :source-paths ["src/cljs"]
-     :compiler     {:main            conway.core
-                    :output-to       "resources/public/js/compiled/app.js"
-                    :optimizations   :advanced
-                    :closure-defines {goog.DEBUG false}
-                    :pretty-print    true
-                    :pseudo-names    true}}
-
-    ]}
+  {:cljs
+   {:source-paths  ["src/cljs"]
+    :dependencies  [[thheller/shadow-cljs "2.4.12"]
+                    [binaryage/devtools "0.9.10"]]
+    :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]}}
 
   )
